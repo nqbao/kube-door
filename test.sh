45fa796c4a8605b4a70e57d46e25c9d6c5ca2b7a
Bao Nguyen <b@nqbao.com>
2017-02-10 21:55:15 -0800

    Use net host for test command so we can see all ports

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python -m kube_door > conf/haproxy.cfg
-docker run --rm -p 80:80 -v `pwd`/conf:/usr/local/etc/haproxy haproxy
+docker run --rm -p -net host -v `pwd`/conf:/usr/local/etc/haproxy haproxy
